3503cdc600d40c5af023c169da69b3ba8d35652f
cristobalnamag <126308802+cristobalnamag@users.noreply.github.com>
2023-04-05 01:24:03 -0600

    Delete listas.py

--- listas.py
+++
@@ -1,9 +0,0 @@
-alumno = ["Cristobal", "Nava", 23, "sagitario", 1.75]
-print(f"el alumno {alumno[0]} {alumno[1]}, tiene {alumno[2]} anios y es signo {alumno[3]}")
-
-print(alumno)
-
-del alumno[3]
-
-alumno.append("sagitario")
-print(alumno)
